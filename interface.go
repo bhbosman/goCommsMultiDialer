03776d188cb8bc7806fbc9599ce7f10497b9ed28
Brendan Bosman <bhbosman@gmail.com>
2022-07-25 22:07:42 +0200

    refactor

--- interface.go
+++ interface.go
@@ -4,8 +4,8 @@ import (
 	"github.com/bhbosman/goCommsDefinitions"
 	"github.com/bhbosman/gocommon/Services/IDataShutDown"
 	"github.com/bhbosman/gocommon/Services/IFxService"
-	"github.com/bhbosman/gocommon/Services/ISendMessage"
 	"github.com/bhbosman/gocommon/messages"
+	"github.com/bhbosman/gocommon/services/ISendMessage"
 	"go.uber.org/fx"
 	"net/url"
 )
